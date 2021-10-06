5436ccb91ea4ff2dc616c59baa508c73c253b795
gnohgnij <congjinghong@gmail.com>
2021-10-06 20:42:03 +0800

    Added FollowPlayer.cs script

---
+++ Edumon/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform player;
+    public float leftLimit, rightLimit, topLimit, bottomLimit;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position + new Vector3(
+			Mathf.Clamp(player.transform.position.x, leftLimit, rightLimit),
+			Mathf.Clamp(player.transform.position.y ,bottomLimit, topLimit),
+			transform.position.z
+		);
+    }
+}
