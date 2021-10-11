46e4027e5cc8ea120375eab0194ec53bfaa686dd
brygoh <72204360+brygoh@users.noreply.github.com>
2021-10-11 21:45:03 +0800

    portal

--- Edumon/Assets/Scripts/FollowPlayer.cs
+++ Edumon/Assets/Scripts/FollowPlayer.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public Transform player;
+    private Vector3 playerPosition;
     public float leftLimit, rightLimit, topLimit, bottomLimit;
 
     // Update is called once per frame
     void Update()
     {
+
         transform.position = player.transform.position + new Vector3(
 			Mathf.Clamp(player.transform.position.x, leftLimit, rightLimit),
 			Mathf.Clamp(player.transform.position.y ,bottomLimit, topLimit),
---
+++ Edumon/Assets/Scripts/Scene1Portal.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scene1Portal : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Player")) {
+            Debug.Log("Here");
+            SceneManager.LoadScene("Gym2"); 
+        }
+    }
+}
