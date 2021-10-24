bb47e3636cd4d743d01f79d22feefaa3bbd79855
gnohgnij <congjinghong@gmail.com>
2021-10-24 15:39:12 +0800

    Update LeaderboardController.cs

--- Edumon/Assets/Scripts/LeaderboardController.cs
+++ Edumon/Assets/Scripts/LeaderboardController.cs
@@ -6,7 +6,7 @@ using UnityEngine.Networking;
 
 public class LeaderboardController : MonoBehaviour
 {
-    private const string attemptURL = "https://cz3003-edumon.herokuapp.com/attempt/email";
+    private const string attemptURL = "https://cz3003-edumon.herokuapp.com/attempt/email/";
     private const string accountURL = "https://cz3003-edumon.herokuapp.com/account";
 
     void Start()
@@ -33,25 +33,22 @@ public class LeaderboardController : MonoBehaviour
                 AccountResponse accountResponse = JsonUtility.FromJson<AccountResponse>(jsonString);
 
                 //List of users
-                User[] userData = accountResponse.data;
-                Debug.Log(userData[0].username);
+                Account[] accounts = accountResponse.data;
 
                 //Get only the users with type == student
-                List<User> students = new List<User>();
-                foreach(User u in userData)
+                List<Account> students = new List<Account>();
+                foreach(Account a in accounts)
                 {
-                    if(u.type == "student")
+                    if(a.type == "student")
                     {
-                        students.Add(u);
+                        students.Add(a);
                     }
                 }
 
-                Debug.Log("students = " + students[0].username);
-
                 //get score from each student
-                foreach(User u in students)
+                foreach(Account a in students)
                 {
-                    StartCoroutine(GetAttemptRequest(attemptURL + u.email));
+                    StartCoroutine(GetAttemptRequest(attemptURL + a.email));
                 }
             }
         }
@@ -72,6 +69,39 @@ public class LeaderboardController : MonoBehaviour
             else
             {
                 Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
+                string jsonString = webRequest.downloadHandler.text;
+                AttemptEmailResponse attemptEmailResponse = JsonUtility.FromJson<AttemptEmailResponse>(jsonString);
+                Attempt[] attempt = attemptEmailResponse.data;
+                
+                //Calculation of total points
+                int totalPoints = 0;
+
+                //key - question_set, value - score
+                Dictionary<string, int> map = new Dictionary<string, int>();
+                foreach(Attempt a in attempt)
+                {   
+                    //if student has multiple attempts on the same qn set, 
+                    //take the attempt with the highest score
+                    if(!map.ContainsKey(a.question_set))
+                        map.Add(a.question_set, a.score);
+                    else
+                    {
+                        int initialScore = map[a.question_set];
+                        if(initialScore < a.score)
+                        {
+                            map.Remove(a.question_set);
+                            map.Add(a.question_set, a.score);
+                        }
+                    }
+                }
+
+                foreach(int score in map.Values)
+                {
+                    totalPoints += score;
+                }
+                
+                Debug.Log(totalPoints);
+
             }
         }
     }
@@ -81,5 +111,35 @@ public class LeaderboardController : MonoBehaviour
 public class AccountResponse
 {
     public bool success;
-    public User[] data;
+    public Account[] data;
+}
+
+[System.Serializable]
+public class Account
+{
+    public string uid;
+    public string email;
+    public string[] unlocked_map;
+    public string username;
+    public string studentid;
+    public string type;
+}
+
+[System.Serializable]
+public class AttemptEmailResponse
+{
+    public bool success;
+    public Attempt[] data;
+}
+
+[System.Serializable]
+public class Attempt
+{
+    public string attempt_id;
+    public string question_set;
+    public string question_group;
+    public string user_email;
+    public int score;
+    public object user_answers;
+    public string completion_status;
 }
