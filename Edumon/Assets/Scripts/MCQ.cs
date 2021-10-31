6f098c0f98bd4065895b8956320ca29293a23f5e
Nicholas Beh <nckb@users.noreply.github.com>
2021-10-31 21:41:01 +0800

    Add further MCQ functionality

--- Edumon/Assets/Scripts/MCQ.cs
+++ Edumon/Assets/Scripts/MCQ.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using UnityEngine.Networking;
 using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 //TODO: Challenge manager to handle 
 
 public class MCQ : MonoBehaviour
 {
     private const string URL = "https://cz3003-edumon.herokuapp.com/";
+    private EventSystem EventSystem;
+    private int AnswerSelection = 0;
     public TextMeshProUGUI QuestionText;
     public Button ButtonA;
     public TextMeshProUGUI ButtonAText;
@@ -33,68 +36,71 @@ public class MCQ : MonoBehaviour
         ButtonC.onClick.AddListener(() => AnswerButtonClicked(3));
         ButtonD.onClick.AddListener(() => AnswerButtonClicked(4));
         ButtonE.onClick.AddListener(() => AnswerButtonClicked(5));
-
+        SubmitButton.onClick.AddListener(SubmitButtonClicked);
+        
         //Disable fifth button as challenges only have 4 options
         ButtonE.gameObject.SetActive(false);
 
-        SubmitButton.onClick.AddListener(TaskOnClick);
-        //GenerateRequest();
+        //Hardcoded question ID here
         StartCoroutine(DisplayQuestionFromId("SM3AlYWKEDIrOo02UJtl"));
     }
 
     // Update is called once per frame
-    void TaskOnClick()
+    void Update()
     {
-        //Output this to console when SubmitButton is clicked
-        GenerateRequest();
+        
+    }
+
+    void SubmitButtonClicked() {
+        //Insert logic here
+        Debug.Log("Submitting option: " + AnswerSelection);
     }
 
     public void RenameTMPLabel(string text, TextMeshProUGUI label) => label.text = text;
 
-    private void EndChallenge() => SceneManager.LoadScene("Gym5");
-
     private IEnumerator DisplayQuestionFromId(string questionId) {
         string questionUrl = URL + "question/" + questionId;
-        Debug.Log(questionUrl);
+        Debug.Log("Retrieving question from: " + questionUrl);
+        using (UnityWebRequest questionRequest = UnityWebRequest.Get(questionUrl)) {
+            yield return questionRequest.SendWebRequest();
 
-        UnityWebRequest questionUwr = UnityWebRequest.Get(questionUrl);
-        yield return questionUwr.SendWebRequest();
-
-        //Add error handling here
-        QuestionGetResult questionResult = JsonUtility.FromJson<QuestionGetResult>(questionUwr.downloadHandler.text);
-        Question question = new Question();
-        question = questionResult.data;
-        
-        RenameTMPLabel(question.question_text, QuestionText);
-        RenameTMPLabel(question.option_one, ButtonAText);
-        RenameTMPLabel(question.option_two, ButtonBText);
-        RenameTMPLabel(question.option_three, ButtonCText);
-        RenameTMPLabel(question.option_four, ButtonDText);
-        //RenameTMPLabel(question.option_five, ButtonEText);
-    }
-
-    void AnswerButtonClicked(int option) {
-        Debug.Log("Button Clicked: " + option);
-    }
-    public void GenerateRequest()
-    {
-        StartCoroutine(ProcessRequest(URL));
-    }
-
-    private IEnumerator ProcessRequest(string url)
-    {
-        using (UnityWebRequest request = UnityWebRequest.Get(url))
-        {
-            yield return request.SendWebRequest();
-
-            if (request.isNetworkError)
-            {
-                Debug.Log(request.error);
-            }
-            else
-            {
-                Debug.Log(UnityWebRequest.Get(url+"question/SM3AlYWKEDIrOo02UJtl"));
+            if (questionRequest.result == UnityWebRequest.Result.ConnectionError) {
+                Debug.Log(questionRequest.error);
+            } else {
+                QuestionGetResult questionResult = JsonUtility.FromJson<QuestionGetResult>(questionRequest.downloadHandler.text);
+                Question question = new Question();
+                question = questionResult.data;
+                
+                RenameTMPLabel(question.question_text, QuestionText);
+                RenameTMPLabel(question.option_one, ButtonAText);
+                RenameTMPLabel(question.option_two, ButtonBText);
+                RenameTMPLabel(question.option_three, ButtonCText);
+                RenameTMPLabel(question.option_four, ButtonDText);
+                //RenameTMPLabel(question.option_five, ButtonEText);
             }
         }
     }
+
+    void AnswerButtonClicked(int option) {
+        EventSystem.current.SetSelectedGameObject(null);
+        AnswerSelection = option;
+        if (option == 1) {
+            ButtonA.Select();
+        }
+        if (option == 2) {
+            ButtonB.Select();
+        }
+        if (option == 3) {
+            ButtonC.Select();
+        }
+        if (option == 4) {
+            ButtonD.Select();
+        }
+        if (option == 5) {
+            ButtonE.Select();
+        }
+        Debug.Log("Answer Button Clicked: " + option);
+    }
+
+    private void EndChallenge() => SceneManager.LoadScene("Gym5");
 }
