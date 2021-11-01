203819ab11ef9ca460f0c67562088d36fbaead42
brygoh <72204360+brygoh@users.noreply.github.com>
2021-11-01 22:43:58 +0800

    fixed dialog bug from jinghong and copied set question ui

--- Edumon/Assets/Scripts/NPCController.cs
+++ Edumon/Assets/Scripts/NPCController.cs
@@ -6,12 +6,24 @@ using UnityEngine.SceneManagement;
 public class NPCController : MonoBehaviour, Interactable
 {
     [SerializeField] Dialog dialog;
+    private bool ManageDialog;
     public void Interact()
     {
         Debug.Log("Interacting with NPC");
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+        ManageDialog = true;
+    }
 
-        if(StateManager.currentGym == "Gym5")
+    public void Update() 
+    {
+        // if (ManageDialog && Input.GetKeyDown(KeyCode.E) && SceneManager.GetActiveScene().name == "Gym5") 
+        // {
+        //     SceneManager.LoadScene("AssignmentChallengeView");
+        // }
+
+        if (ManageDialog && Input.GetKeyDown(KeyCode.E) && StateManager.currentGym == "Gym5") 
+        {
             SceneManager.LoadScene("AssignmentChallengeView");
+        }
     }
 }
