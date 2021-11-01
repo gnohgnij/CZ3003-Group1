998f2d61a4f0e6ae636fc963d9f2f8f6479fc29d
gnohgnij <congjinghong@gmail.com>
2021-11-01 16:23:51 +0800

    Added AssignmentChallengeView

---
+++ Edumon/Assets/Scripts/AssignmentChallenge/AssignmentChallengeController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class AssignmentChallengeController : MonoBehaviour
+{
+    public void Btn_View_Assignment_Clicked()
+    {
+        SceneManager.LoadScene("Assignment");
+    }
+
+    public void Btn_View_Challenge_Clicked()
+    {
+        SceneManager.LoadScene("Challenge");
+    }
+}
